c62fc90c8a4b76b3991da14779d1fe043f986f54
renannunesok <renannunesok@gmail.com>
2011-12-27 00:58:43 -0200

    Pequenas Alteracoes

--- mail.php
+++ mail.php
@@ -1,4 +1,5 @@
 <?php
+	
 // Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
 require("phpmailer/class.phpmailer.php");
 
@@ -59,4 +60,5 @@ if ($enviado) {
 	echo "Não foi possível enviar o e-mail.<br /><br />";
 	echo "<b>Informações do erro:</b> <br />" . $mail->ErrorInfo;
 }
+
 ?>
